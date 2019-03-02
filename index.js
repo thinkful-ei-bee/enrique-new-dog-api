296e248aa0a254e0e191aeaaa617659c082e2c56
Enrique Montemayor <eemontemayor@live.com>
2019-03-02 12:48:45 -0600

    finished breed function

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const STORE = {
     userNumber: 0,
     dogArray: [],
     breedSearch: [],
+    breedArray: []
 
 }
 
 function handleNumberSubmit() {
-    $('form').submit('user-number', event => {
+    $('#js-number-entry').submit('.user-number', event => {
       event.preventDefault();
       if ($('.user-number').val()=== ''){
          $('.user-number').val(3);
@@ -24,34 +25,45 @@ function handleNumberSubmit() {
   })
 }
 
-
 function getDogImages(number) {
   fetch(`https://dog.ceo/api/breeds/image/random/${number}`)
     .then(response => response.json())
-    .then(jsonObj => displayResults(jsonObj))
+    .then(jsonObj => displayNumber(jsonObj))
     .catch(error => alert('Something happened! Try again.'));
 }
 
-function displayResults(x) {
+function displayNumber(x) {
 STORE.dogArray = x.message;
-  $('.results').replaceWith(makeHtml());
+  $('.results').replaceWith(makeHtml(STORE.dogArray));
 }
 
-function makeHtml(){
-    return STORE.dogArray.map(i => `<img src="${i}" class="results">` );
+function makeHtml(arr){
+    return arr.map(i => `<img src="${i}" class="results">` );
 } 
 
-// function handleBreedSubmit(){
-//   $('form').submit('.breed-search', event =>{
-//     event.preventDefault();
-//     STORE.breedSearch = $('.breed-search').val();
-//     console.log(STORE.breedSearch);
-//   })
-// }
+function handleBreedSubmit(){
+  $('#js-breed-entry').submit('.breed-search', event =>{
+    event.preventDefault();
+    STORE.breedSearch = $('.breed-search').val();
+    console.log(STORE.breedSearch);
+    getDogBreed(STORE.breedSearch);
+  })
+}
+function getDogBreed(breed){
+  fetch(`https://dog.ceo/api/breed/${breed}/images`)
+      .then(response => response.json())
+      .then(jsonObj => displayBreed(jsonObj))
+      .catch(error => alert('Something happened! Try again some other time.'));
+}
+function displayBreed(x){
+  STORE.breedArray = x.message[0];
+  console.log(STORE.breedArray);
+  $('.results').replaceWith(`<img src="${STORE.breedArray}" class="results">`);
 
+}
 
 $(function() {
   console.log('App loaded! Waiting for submit!');
   handleNumberSubmit();
-  // handleBreedSubmit();
+  handleBreedSubmit();
 }); 

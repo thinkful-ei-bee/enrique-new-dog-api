3ba7982696e045745fa8d7df4051ddbc50cd7e86
Enrique Montemayor <eemontemayor@live.com>
2019-03-02 12:07:34 -0600

    added default value for form input

--- index.js
+++ index.js
@@ -4,18 +4,23 @@
 const STORE = {
     userNumber: 0,
     dogArray: [],
+    breedSearch: [],
+
 }
 
-function watchForm() {
-    $('form').on('submit', event => {
+function handleNumberSubmit() {
+    $('form').submit('user-number', event => {
       event.preventDefault();
-      if ($('.user-number').val() > 50) {
+      if ($('.user-number').val()=== ''){
+         $('.user-number').val(3);
+     }
+      if ($('.user-number').val() > 50 || $('.user-number').val() < 1) {
           return alert('Please choose a valid number');
-      }else {
+      } 
           STORE.userNumber = $('.user-number').val();
           console.log(STORE.userNumber);
           getDogImages(STORE.userNumber);
-    }
+    
   })
 }
 
@@ -36,8 +41,17 @@ function makeHtml(){
     return STORE.dogArray.map(i => `<img src="${i}" class="results">` );
 } 
 
+// function handleBreedSubmit(){
+//   $('form').submit('.breed-search', event =>{
+//     event.preventDefault();
+//     STORE.breedSearch = $('.breed-search').val();
+//     console.log(STORE.breedSearch);
+//   })
+// }
+
 
 $(function() {
   console.log('App loaded! Waiting for submit!');
-  watchForm();
+  handleNumberSubmit();
+  // handleBreedSubmit();
 }); 
